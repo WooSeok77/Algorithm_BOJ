50be4fcf86875bbeda529a181ab77117a4858422
Æsir <wi8tkf@naver.com>
2023-03-22 01:20:28 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner num = new Scanner(System.in);
+		double a = num.nextDouble();
+		double b = num.nextDouble();
+		System.out.println(a/b);
+		
+	}
+
+}
