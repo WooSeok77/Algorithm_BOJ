0320fb86b9e5e26c76fac03241edbb4d75543603
Æsir <wi8tkf@naver.com>
2023-04-03 17:45:34 +0900

    [Bronze I] Title: 평균, Time: 256 ms, Memory: 18784 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -6,21 +6,23 @@ public class Main {
         int count;
         Scanner sc = new Scanner(System.in);
 
-        count = sc.nextInt(); //시험본 과목 개수 입력.
+        count = sc.nextInt(); 
 
         double avg =0 ;
-        double[] a = new double[count]; //과목 개수에 따른 점수배열 크기 지정
+        double[] a = new double[count]; 
 
         double max = a[0];
         for(int i = 0; i < a.length ; i++) {
             a[i] = sc.nextInt();
-            if(max < a[i]) {
+
+        }
+        for(int i = 0; i < a.length ; i++) {
+            if(max < a[i])
                 max = a[i];
-            }
         }
 
         for(int m = 0; m <a.length; m++) {
-            avg += (((a[m] / max) * 100) / count);
+            avg += (a[m] / max * 100) / count;
         }
         System.out.println(avg);
     }
