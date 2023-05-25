c8501b8551cbee705839d09c5f67f99a19c5cc76
Æsir <wi8tkf@naver.com>
2023-05-25 15:44:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int t = scanner.nextInt();
+        int n;
+        
+        int[] output = new int[11]; 
+        output[1] = 1; 
+        output[2] = 2; 
+        output[3] = 4; 
+        
+        for(int i=0; i<t; i++){
+            n = scanner.nextInt();
+            
+            for(int j=4; j<=n; j++){
+                output[j] = output[j-1] + output[j-2] + output[j-3];
+            }
+            System.out.println(output[n]);
+        }
+        
+ 
+    }
+ 
+}
